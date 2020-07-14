f006c474cb87c105af4a5e1aeb50b7366864126b
Patrick Parkinson <patrick.parkinson@manchester.ac.uk>
2020-07-14 21:14:11 +0100

    Changed to declarative tables rather than reflective/automap tables.

--- db_orm.py
+++ db_orm.py
@@ -2,12 +2,12 @@
 # Author : Patrick Parkinson <patrick.parkinson@manchester.ac.uk>
 # Date : 30/06/2020
 
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, relationship
+from sqlalchemy import create_engine, Integer, ForeignKey, Text, String, Column, DateTime, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
 import configparser
 import os
-
+from datetime import datetime
 # Get current path (required for locating config.ini)
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,23 +16,82 @@ config = configparser.ConfigParser()
 config.read(os.path.join(package_directory, 'config.ini'))
 
 # Create base autoloader
-Base = automap_base()
-
+decBase = declarative_base()
 # Connect to the database using credentials from config.ini
 engine = create_engine('mysql+mysqlconnector://%s:%s@db.oms-lab.org:3306/bdne' % (
     config.get('DATABASE', 'user'), config.get('DATABASE', 'pass')))
 
-# reflect the tables
-Base.prepare(engine, reflect=True)
 
 # Set up the tables
-Measurement = Base.classes.measurement
-Object = Base.classes.object
-Entity = Base.classes.entity
-Sample = Base.classes.sample
-Experiment = Base.classes.experiment
-EntityGroup = Base.classes.entityGroup
-# Set up metadata engine
-Metadata = Base.classes.metadata
+
+class Object(decBase):
+    """Object SQLAlchemy Table"""
+    __tablename__ = 'object'
+
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    entity_id = Column(Integer, ForeignKey('entity.ID'))
+    created = Column(DateTime, default=datetime.now())
+
+    entity = relationship("Entity", back_populates="objects")
+    measurement = relationship("Measurement", back_populates="object")
+
+
+class Metadata(decBase):
+    """Metadata SQLAlchemy Table"""
+    __tablename__ = 'metadata'
+
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    experiment_id = Column(Integer, ForeignKey('experiment.ID'))
+    key = Column(Text(length=255))
+    value = Column(Text(length=65535))
+
+    experiment = relationship('Experiment', back_populates='meta')
+
+
+class Entity(decBase):
+    """Entity SQLAlchemy Table"""
+    __tablename__ = 'entity'
+
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    sampleID = Column(Integer, ForeignKey('sample.ID'))
+
+    objects = relationship('Object', back_populates='entity')
+    sample = relationship('Sample', back_populates='entities')
+
+
+class Sample(decBase):
+    __tablename__ = 'sample'
+
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    material = Column(String(50))
+    supplier = Column(String(50))
+    preparation_date = Column(DateTime)
+    substrate = Column(String(50))
+    preparation_method = Column(String(50))
+
+    entities = relationship('Entity', back_populates='sample')
+
+
+class Experiment(decBase):
+    __tablename__ = 'experiment'
+
+    ID = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(50))
+
+    meta = relationship('Metadata', back_populates='experiment')
+
+
+class Measurement(decBase):
+    __tablename__ = 'measurement'
+
+    ID = Column(Integer, primary_key=True)
+    experiment_ID = Column(Integer, ForeignKey('experiment.ID'), autoincrement=True)
+    object_ID = Column(Integer, ForeignKey('object.ID'))
+    data = Column(LargeBinary(length=16777215))
+    created = Column(DateTime, default=datetime.now())
+
+    object = relationship('object', back_populates='measurement')
+
+
 # Set up a session
 session = Session(engine)
