258294ee43e02fff652042a3b050e25ef44ebd0c
Patrick Parkinson <patrick.parkinson@manchester.ac.uk>
2020-10-19 21:57:37 +0100

    Entirely rewritten example code in main.py using data_structures approach

--- main.py
+++ main.py
@@ -1,61 +1,47 @@
-# Testing code for accessing data in AWS database
+# Test code for BDNE
+# Author : Patrick Parkinson <patrick.parkinson@manchester.ac.uk>
 
-import numpy as np
 import matplotlib.pyplot as plt
-from db_orm import *
-
-
-def fetch_random(all_measurements: np.ndarray, n: int) -> np.ndarray:
-    # Fetch a set of n measurement data from the database
-    tofetch = np.random.randint(1, len(all_measurements), n)
-    selectedmeasurementids = [i.item() for i in all_measurements[tofetch]]
-    # New sql query to collect measurements from the data
-    getexp = session.query(Object.ID, Measurement.data). \
-        join(Experiment).join(Object). \
-        filter(Measurement.ID.in_(selectedmeasurementids))
-    return getexp.all()
-
-
-def show_images(pic_data: np.ndarray):
-    # iterate over each image and show it
-    for i in range(len(pic_data)):
-        data = pic_data[i][1]
-        plt.subplot(3, 4, i + 1)
-        plt.imshow(data)
-        plt.xticks([])
-        plt.yticks([])
-        plt.axis('image')
-        plt.title(pic_data[i][0])
-    plt.show()
-    plt.close()
-
-
-def show_spectra(spectra_data: np.ndarray):
-    # iterate over each spectra and show it
-    for i in range(len(spectra_data)):
-        data = spectra_data[i][1]
-        plt.subplot(3, 4, i + 1)
-        plt.plot(data)
-        plt.yticks([])
-        plt.title(spectra_data[i][0])
-    plt.show()
-    plt.close()
-
+import data_structures
 
 ####################################################################
+# Set up a wire collection
+w = data_structures.WireCollection()
+# Populate from the database, sample ID 25
+w.loadsample(25)
+# List how many wires samples
+print('Wire Collection has {} wires'.format(len(w.db_ids)))
 
-displayType = 'spectra'
+#############
+# Get one wire to test at random
+wire = w.sample(1)
+# State which one samples
+print('Selected wire ID {}'.format(wire.db_id))
+# List what data this wire has
+print(wire.experiments())
 
-# Create sql select
-stm = session.query(Measurement.ID).join(Experiment). \
-    filter(Experiment.type == displayType)
+##############
+# Get all wire lengths
+lengths = w.get('l')
+# State how many obtained
+print('Obtained measurement set with {} measurements'.format(len(lengths.db_ids)))
 
-all_pics = stm.all()
-# Randomly select n
-all_pics = np.array([i[0] for i in all_pics])
-dr = fetch_random(all_pics, 12)
-
-if displayType == 'pic':
-    show_images(dr)
-elif displayType == 'spectra':
-    show_spectra(dr)
+####################################################################
+# Plot the image and spectra
+plt.clf()
+plt.subplot(221)
+plt.imshow(wire.get('pic'))
+plt.title('Pic {}'.format(wire.db_id))
+plt.subplot(222)
+plt.plot(wire.get('spectra'))
+plt.title('Spectra')
+plt.ylabel('PL (arb)')
+plt.xlabel('Spectra (arb)')
+plt.subplot(212)
+plt.hist(lengths.collect())
+yl = plt.ylim()
+plt.plot(wire.get('l')*[1,1],yl)
+plt.title('Lengths of {} wires'.format(len(w.db_ids)))
+plt.xlabel('Length (um)')
+plt.ylabel('Frequency')
+plt.show()
